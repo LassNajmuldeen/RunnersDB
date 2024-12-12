cd8ed6b526fc2cfbb064599c81ea048a252fc757
LassNajmuldeen <lass.najm@gmail.com>
2024-12-12 23:29:27 +0100

    Downloaded json files

--- RunnersDB.py
+++ RunnersDB.py
@@ -1,1 +1,10 @@
 import json
+
+# Load JSON data from files
+def load_json(file_path):
+    try:
+        with open(file_path, "r") as f:
+            return json.load(f)
+    except Exception as e:
+        print(f"Error loading file {file_path}: {e}")
+        return {}
